5352cd5f3e53a27747c8afa2765f44ff073c22b0
bernardo607 <bernardo.segui@gmail.com>
2021-09-27 21:07:17 -0300

    config heroku

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ app.use(express.static('public'));
 
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname + './views/home.html');
 });
 /* Register */
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(__dirname + './views/register.html');
 });
 app.post('/register', (req,res)=> {
     res.send('Ya te registraste campeón');
 });
 /* Login */
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(__dirname + './views/login.html');
 });
 app.post('/login', (req,res)=>{
     res.send('Ya te logueaste campeón');
