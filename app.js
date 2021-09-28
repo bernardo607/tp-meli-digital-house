0d51f568522b92648857847a7faaec2f9403a892
bernardo607 <bernardo.segui@gmail.com>
2021-09-27 21:18:09 -0300

    config heroku

--- app.js
+++ app.js
@@ -6,23 +6,23 @@ app.use(express.static('public'));
 
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + './views/home.html');
+    res.sendFile(__dirname + '/views/home.html');
 });
 /* Register */
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + './views/register.html');
+    res.sendFile(__dirname + '/views/register.html');
 });
 app.post('/register', (req,res)=> {
     res.send('Ya te registraste campeón');
 });
 /* Login */
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + './views/login.html');
+    res.sendFile(__dirname + '/views/login.html');
 });
 app.post('/login', (req,res)=>{
     res.send('Ya te logueaste campeón');
 });
 
 app.listen(process.env.PORT || 3000,function () {
-    console.log(`Servidor corriendo en puerto 3000`)
+    console.log('Servidor corriendo en puerto 3000')
   });
